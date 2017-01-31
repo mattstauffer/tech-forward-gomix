d49c1d66076474a220d9e31b08de3e1fbb781244
System <none>
2017-01-31 13:55:56 +0000

    🌻🚘 Updated with Gomix

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Sharing tech-focused organizations and tools working for social progress.
 
 ## To do
-- [ ] Figure out the timeouts; is there a rate limit on the Google Sheet? If so, can we cache it?
+- [x] Figure out the timeouts; is there a rate limit on the Google Sheet? If so, can we cache it? (race condition, not timeout)
 - [ ] Bring over any files we need from Phantom
 - [ ] Determine whether we can survive without a build step; if not, move source files into the built CSS files here so we have the originals
 - [ ] Tests
@@ -10,3 +10,12 @@ Sharing tech-focused organizations and tools working for social progress.
 - [ ] Redirect old GitHub
 - [ ] Hook up a github project for backups
 - [ ] Set up Google Forms and properly lock down the spreadsheet
+- [ ] How do we handle image uploads?
+
+## Getting started
+
+- `cp .env.example .env`
+- Open `.env` in your editor. You may need to fill in some values for your dev. See comments in that file.
+- `npm install`
+- `npm start`
+- `open http://localhost:3030`
---
+++ src/spreadsheet/importer.js
@@ -0,0 +1,74 @@
+const GoogleSpreadsheets = require('google-spreadsheets')
+const _ = require('lodash')
+const Promise = require('promise')
+
+const sheets = {
+  'orgs': {
+    'number': 0,
+    'range': 'R1C1:R99C9'
+  },
+  'tools': {
+    'number': 1,
+    'range': 'R1C1:R99C9'
+  },
+  'resources': {
+    'number': 2,
+    'range': 'R1C1:R99C5'
+  }
+}
+
+// Get and transform the value of a given sheet
+const getSheet = function getSheet(sheetName) {
+  return new Promise((resolve, reject) => {
+    if (!(sheetName in sheets)) {
+      return reject('Not a valid sheet')
+    }
+
+      // "spreadsheet key" is the long ID in the sheet's URL 
+    GoogleSpreadsheets({
+      key: "1Kv7neJBJcg_ivrft2Wgd0Qs4DtpG7KCkZlnC3b234N8"
+    }, function(err, spreadsheet) {
+      const sheet = sheets[sheetName]
+      
+      spreadsheet.worksheets[sheet.number].cells({
+        range: sheet.range
+      }, function(err, result) {
+        resolve(transformSheet(result.cells))
+      })
+    })
+  })
+}
+
+// Transform a sheet from Google into a JSON format
+// @todo: Delete approved
+function transformSheet(sheetFromGoogle)
+{
+  let headers = _.map(sheetFromGoogle[1], 'value')
+  delete sheetFromGoogle[1]
+  
+  return _.chain(sheetFromGoogle)
+    .map((row) => { return makeRow(row, headers) })
+    .filter((row) => { return row.approved == 1 })
+    .sortBy('name')
+    .values()
+}
+
+// Use the headers row to map the values from each row
+// to the appropriate JSON object property
+function makeRow(row, headers)
+{
+  return _.reduce(headers, (obj, header, index) => {
+    obj[header] = cell(row, index)
+    return obj
+  }, {})
+}
+
+// Get the value of a 1-based cell based on its 0-based index,
+// allowing for null values
+function cell(row, index) {
+  return row[index + 1] ? row[index + 1]['value'] : null
+}
+
+module.exports = {
+  getSheet
+}
