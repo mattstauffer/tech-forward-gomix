ea288c4cec430955d2646a4650f41d4b5f5a22a1
Matt Stauffer <matt@tighten.co>
2017-02-04 23:50:00 -0500

    bring back slug and location strings

--- src/spreadsheet/importer.js
+++ src/spreadsheet/importer.js
@@ -29,9 +29,7 @@ const sheets = {
 // Get and transform the value of a given sheet
 const getSheet = function getSheet(sheetName) {
   return new Promise((resolve, reject) => {
-    if (!(sheetName in sheets)) {
-      return reject('Not a valid sheet')
-    }
+    if (!(sheetName in sheets)) return reject('Not a valid sheet')
 
     let cacheKey = 'sheet_' + sheetName
     let cacheResponse = mcache.get(cacheKey)
@@ -60,19 +58,18 @@ const getSheet = function getSheet(sheetName) {
 }
 
 // Transform a sheet from Google into a JSON format
-// @todo: Delete approved
 function transformSheet(sheetFromGoogle)
 {
   let headers = _.map(sheetFromGoogle[1], 'value')
   delete sheetFromGoogle[1]
 
-  var thing = _.map(sheetFromGoogle, (row) => { return makeRow(row, headers) })
-  thing = _.filter(thing, (row) => { return row.approved == 1 })
-  thing = _.sortBy(thing, 'name')
-  return thing
+  let output = _.map(sheetFromGoogle, (row) => { return makeRow(row, headers) })
+  output = _.filter(output, (row) => { return row.approved == 1 })
+  output = _.sortBy(output, 'name')
+  return output
 
-  // for some reason we're getting a lodash wrapper result from this... so temp overriding
-
+  // for some reason we're getting a lodash wrapper result from this... so temp
+  // overriding with the return above
   return _.chain(sheetFromGoogle)
     .map((row) => { return makeRow(row, headers) })
     .filter((row) => { return row.approved == 1 })
@@ -84,16 +81,57 @@ function transformSheet(sheetFromGoogle)
 // to the appropriate JSON object property
 function makeRow(row, headers)
 {
+  return decorate(mapRowFromGoogle(row, headers))
+}
+
+// Map the result from Google sheets to a usable JSON object
+function mapRowFromGoogle(row, headers)
+{
   return _.reduce(headers, (obj, header, index) => {
     obj[header] = cell(row, index)
     return obj
   }, {})
 }
 
+// Decorate the API result with slug and readable location string
+function decorate(row)
+{
+  row.slug = slugify(row.name)
+  row.location = buildLocationString(row)
+
+  return row
+}
+
+// Convert a string to a slug
+function slugify(string)
+{
+  return string.toString().toLowerCase().trim()
+    .replace(/&/g, '-and-')         // Replace & with 'and'
+    .replace(/[\s\W-]+/g, '-')      // Replace spaces, non-word characters and dashes with a single dash (-)
+}
+
 // Get the value of a 1-based cell based on its 0-based index,
 // allowing for null values
 function cell(row, index) {
   return row[index + 1] ? row[index + 1]['value'] : null
+}
+
+// Build a readable location string based on optional location fields
+function buildLocationString(item)
+{
+  if (item.locationAddress && item.locationCity && item.locationState) {
+    return item.locationAddress + ', ' + item.locationCity + ', ' + item.locationState
+  }
+
+  if (item.locationCity && item.locationState) {
+    return item.locationCity + ', ' + item.locationState
+  }
+
+  if (item.locationState) {
+    return item.locationState
+  }
+
+  return null
 }
 
 module.exports = {
